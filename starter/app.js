d44d39da26e11fbdbc167a92149141a659f4acc9
mudit vijay <98879664+Mudit-vijay@users.noreply.github.com>
2025-08-24 11:27:48 +0530

    improved

--- starter/app.js
+++ starter/app.js
@@ -1,32 +1,39 @@
-const connectdb = require('./db/connect')
-const express = require('express')
-const cors = require('cors')
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectdb from "./db/connect.js";
+import groupRouter from "./routes/groups.js";
+import taskRouter from "./routes/task.js";
+// import notFound from "./middleware/error-handler.js";
+
+dotenv.config();
+
 const app = express();
-const group = require('./routes/groups')
-const task = require('./routes/task')
-// const notFOund = require('./middleware/error-handler')
-require('dotenv').config()
-app.use(cors({
-    origin: ['https://task-manager-xp1g.onrender.com'],
-    credentials: true
-}))
-//middleware
-app.use(express.static('./public'))
-app.use(express.json())
-app.use('/api/v1/task', task);
-app.use('/api/v1/group', group);
-// app.use(notFOund)
+
+app.use(
+    cors({
+        origin: ["https://task-manager-xp1g.onrender.com"],
+        credentials: true,
+    })
+);
+
+// middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+app.use("/api/v1/task", taskRouter);
+app.use("/api/v1/group", groupRouter);
+// app.use(notFound);
+
 const start = async () => {
     try {
-        await connectdb(process.env.MONGO_URI)
+        await connectdb(process.env.MONGO_URI);
         app.listen(3000, () => {
             console.log("server is listening on port 3000");
-
-        })
+        });
     } catch (err) {
-        console.log(err);
-
+        console.error(err);
     }
-}
-start()
+};
 
+start();
