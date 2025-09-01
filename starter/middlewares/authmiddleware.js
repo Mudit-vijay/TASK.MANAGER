e37668c617fadd7bbb24e9e23179db0ef5663620
mudit vijay <98879664+Mudit-vijay@users.noreply.github.com>
2025-09-01 16:02:14 +0530

    Update authmiddleware.js
    auth middleware debugging

--- starter/middlewares/authmiddleware.js
+++ starter/middlewares/authmiddleware.js
@@ -1,7 +1,11 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
+    console.log("request received");
+    console.log(req.body);
+    console.log(req.cookies);
     const token = req.cookies?.token;
+    console.log(token)
     if (!token) return res.status(401).json({ msg: "No token found" });
 
     try {
@@ -14,3 +18,4 @@ const authMiddleware = (req, res, next) => {
 };
 
 export default authMiddleware;
+
