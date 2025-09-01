4562663f1e80c4690d900c8316e90b4191cbd2c5
mudit vijay <98879664+Mudit-vijay@users.noreply.github.com>
2025-09-01 19:04:14 +0530

    fix api call of group

--- Gateway/controllers/group.js
+++ Gateway/controllers/group.js
@@ -8,8 +8,10 @@ app.use(express.json());  // To parse JSON body
 // ✅ Get all group
 //you have to send the user id to access all the groups 
 app.get('/group', async (req, res) => {
-const token=req.params;
-console.log(token)
+    console.log("request received on gateway")
+console.log("req.params" +req.params);
+    const token=req.params;
+console.log("token"+token)
   try {
       const response = await api.groupapi.get(`/groups/${token}`, );
     console.log(response.data);
