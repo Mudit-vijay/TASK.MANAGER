124682cbac7ba58c94c2468f781c5b00afd2e51c
mudit vijay <98879664+Mudit-vijay@users.noreply.github.com>
2025-09-01 16:23:01 +0530

    Update store.jsx
    set up redux for token slice

--- FRONTEND/features/store.jsx
+++ FRONTEND/features/store.jsx
@@ -2,11 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./auth/auth-slice";
 import userIdReducer from "./userID/userId-slics";
+import tokenslice from "./token-slice.jsx";
 const store = configureStore({
   reducer: {
     auth: authReducer,
     userId: userIdReducer,
+    token:tokenslice,
   },
 });
 
 export default store;
+
