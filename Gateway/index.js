113b9c818fc54107a45751fa51b6112c5f1d6d50
mudit vijay <98879664+Mudit-vijay@users.noreply.github.com>
2025-09-15 08:26:55 +0530

    completed first phase of project

--- Gateway/index.js
+++ Gateway/index.js
@@ -6,7 +6,7 @@ const tasks = require('./controllers/tasks.js')
 const group = require('./controllers/group.js')
 
 app.use(cors({
-    // origin: 'https://task-manager-1-5jlg.onrender.com',
+    origin: 'https://task-manager-1-5jlg.onrender.com',
     origin:"http://localhost:5173",
     credentials: true,
 }));
