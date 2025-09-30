2976b5d5b151250e1bb06c2ca8cb8c473064cc8e
mudit vijay <98879664+Mudit-vijay@users.noreply.github.com>
2025-09-30 23:14:40 +0530

    check all routes for dev

--- login_Services/index.js
+++ login_Services/index.js
@@ -12,13 +12,7 @@ const router = require('./routers/routes.js');
 
 app.use(express.json());
 app.use(cors({
-// <<<<<<< HEAD
     origin: 'https://task-manager-xp1g.onrender.com',
-    // origin: "https://localhost:8080/",
-// =======
-    // origin: 'https://task-manager-xp1g.onrender.com',
-//     origin: "https://localhost:8080/",
-// >>>>>>> 33251607f065c3833afaddc6a006e9a2ab46db2f
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
--- login_Services/schemas/customer_Schema.js
+++ login_Services/schemas/customer_Schema.js
@@ -17,7 +17,7 @@ const customer_Schema = new mongoose.Schema({
     },
     role: {
         type: String,
-        default: "user",
+        default: "USER",
         enum: ["CREATOR", "ADMIN", "USER"],
     }
 })
