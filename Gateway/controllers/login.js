73b9bb32c194581d3cb9e4aac6b0599251815da5
mudit vijay <98879664+Mudit-vijay@users.noreply.github.com>
2025-12-23 08:32:37 +0530

    fix oauth gateway token issue

--- Gateway/controllers/login.js
+++ Gateway/controllers/login.js
@@ -45,7 +45,8 @@ app.post('/oauthcreation', async (req, res) => {
 
     try {
         // Extract token from request headers
-        const header = req.headers["authorization"];
+        const header = req.headers["Authorization"];
+        console.log(req.headers);
         console.log(header);
         if (!header) {
             return res.status(401).json({ message: "No token provided" });
