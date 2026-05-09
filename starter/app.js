0117da6c91da8845f6594dd89b143403651c382c
mudit vijay <98879664+Mudit-vijay@users.noreply.github.com>
2026-05-10 01:21:04 +0530

    feat/ implemented new feature regarding phase 2

--- starter/app.js
+++ starter/app.js
@@ -1,19 +1,30 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import compression from "compression";
 import connectdb from "./db/connect.js";
 import groupRouter from "./routes/groups.js";
 import taskRouter from "./routes/task.js";
-// import notFound from "./middleware/error-handler.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(helmet());
+app.use(compression());
+
 app.use(
     cors({
-        // origin: ["https://task-manager-xp1g.onrender.com"],
-        origin: "https://localhost:8080/",
+        origin: [
+            "https://task-manager-xp1g.onrender.com",
+            "http://localhost:8080",
+            "http://localhost:5173",
+            "https://backend-a-tvul.onrender.com",
+            "https://backend-b-wxdw.onrender.com",
+            "https://task-manager-1-5jlg.onrender.com"
+        ],
         credentials: true,
     })
 );
@@ -21,11 +32,12 @@ app.use(
 // middleware
 app.use(express.static("./public"));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1/task", taskRouter);
 app.use("/api/v1/group", groupRouter);
-// app.use(notFound);
-const port = 9000
+
+const port = process.env.PORT || 9000;
 const start = async () => {
     try {
         await connectdb(process.env.MONGO_URI);
@@ -33,7 +45,7 @@ const start = async () => {
             console.log(`server is listening on port ${port}`);
         });
     } catch (err) {
-        console.error(err);
+        console.log(err);
     }
 };
 
