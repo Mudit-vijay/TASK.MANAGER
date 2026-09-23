5cf7b99fd84eba8de331ee237cdfa53f9adec173
mudit vijay <98879664+Mudit-vijay@users.noreply.github.com>
2026-09-23 06:41:56 +0530

    Modify connect.js

--- starter/db/connect.js
+++ starter/db/connect.js
@@ -2,10 +2,15 @@ import mongoose from "mongoose";
 
 const connectDB = async (url) => {
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
     console.log("✅ MongoDB connected successfully");
   } catch (error) {
-    console.error("❌ Error connecting to MongoDB", error);
+    console.error("❌ Error connecting to MongoDB", error.name, error.code || "");
     process.exit(1); // Exit the process if connection fails
   }
 };
